2fe1ecc92700f0d6355b585c40280af5245d9981
Joel Martinez <joelmartinez@gmail.com>
2012-08-20 21:07:22 -0400

    added sample project

---
+++ Sample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var response = ChuckNorris.API.Random(exclude: new string[] {"explicit"});
+            var joke = response.Result;
+
+            Console.WriteLine(joke.Text);
+        }
+    }
+}
--- icndb-csharp/API.cs
+++ icndb-csharp/API.cs
@@ -4,7 +4,7 @@ namespace ChuckNorris
 {
     public static class API
     {
-        public static Task<Result> Random(string[] limitTo = null, string[] exclude = null)
+        public static Task<Joke> Random(string[] limitTo = null, string[] exclude = null)
         {
             string url = "http://api.icndb.com/jokes/random";
 
@@ -13,7 +13,7 @@ namespace ChuckNorris
             if (toExclude) url += string.Format("&exclude=[{0}]", string.Join(",", exclude));
             if (toInclude) url += string.Format("&limitTo=[{0}]", string.Join(",", limitTo));
 
-            return WebHelper.Json<Result>(url);
+            return WebHelper.Json<Result>(url).ContinueWith(t => t.Result.Value);
         }
     }
 }
