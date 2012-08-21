979e865070b1d2d0dfab035f39fecc828c5752a4
Joel Martinez <joelmartinez@gmail.com>
2012-08-20 20:30:31 -0400

    added API result classes

---
+++ icndb-csharp/Joke.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace ChuckNorris
+{
+    [DataContract]
+    public class Joke
+    {
+        [DataMember(Name="id")]
+        public string Id { get; set; }
+
+        [DataMember(Name="joke")]
+        public string Text { get; set; }
+    }
+}
---
+++ icndb-csharp/Result.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Serialization;
+
+namespace ChuckNorris
+{
+    /// <summary>
+    /// Sample:
+    /// { "type": "success", "value": { "id": 268, "joke": "Time waits for no man. Unless that man is Chuck Norris." } }
+    /// </summary>
+    [DataContract]
+    public class Result
+    {
+        [DataMember(Name="type")]
+        public string Type { get; set; }
+
+        [DataMember(Name = "value")]
+        public Joke Value { get; set; }
+    }
+
+}
