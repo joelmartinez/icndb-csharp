fd922ca0fcc564635a288dd2c47474df1bc26021
Joel Martinez <joelmartinez@gmail.com>
2012-08-20 20:49:59 -0400

    API class with random method

---
+++ icndb-csharp/API.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+
+namespace ChuckNorris
+{
+    public static class API
+    {
+        public static Task<Result> Random(string[] limitTo = null, string[] exclude = null)
+        {
+            string url = "http://api.icndb.com/jokes/random";
+
+            bool toInclude = limitTo != null, toExclude = exclude != null;
+
+            if (toExclude) url += string.Format("&exclude=[{0}]", string.Join(",", exclude));
+            if (toInclude) url += string.Format("&limitTo=[{0}]", string.Join(",", limitTo));
+
+            return WebHelper.Json<Result>(url);
+        }
+    }
+}
+        
