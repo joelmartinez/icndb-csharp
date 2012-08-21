9a1ebc0b4c9686b5aa430709d353d85e6c831cb1
Joel Martinez <joelmartinez@gmail.com>
2012-08-20 20:24:04 -0400

    initial commit, with webhelper

---
+++ icndb-csharp/WebHelper.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChuckNorris
+{
+    internal static class WebHelper
+    {
+        /// <summary>Does an HTTP get on the supplied URL</summary>
+        public static Task<T> Json<T>(string url)
+        {
+            return Json<T>(url, string.Empty);
+        }
+
+        /// <summary>Does an HTTP get if no post data is supplied, otherwise, a post</summary>
+        public static Task<T> Json<T>(string url, string postData)
+        {
+            var tcs = new TaskCompletionSource<T>();
+            var request = (HttpWebRequest)WebRequest.Create(url);
+
+            if (!string.IsNullOrWhiteSpace(postData))
+            {
+                request.AllowWriteStreamBuffering = true;
+                request.Method = "POST";
+                request.ContentLength = postData.Length;
+                request.ContentType = "application/x-www-form-urlencoded";
+
+                StreamWriter writer = new StreamWriter(request.GetRequestStream());
+                writer.Write(postData);
+            }
+
+            try
+            {
+                request.BeginGetResponse(iar =>
+                {
+                    HttpWebResponse response = null;
+                    try
+                    {
+                        response = (HttpWebResponse)request.EndGetResponse(iar);
+                        if (response.StatusCode == HttpStatusCode.OK)
+                        {
+                            var sreader = new StreamReader(response.GetResponseStream());
+                            var result = sreader.ReadToEnd();
+
+                            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(T));
+                            byte[] bytes = Encoding.UTF8.GetBytes(result);
+                            using (var stream = new MemoryStream(bytes))
+                            {
+                                var deserialized = serializer.ReadObject(stream);
+
+                                tcs.SetResult((T)deserialized);
+                            }
+                        }
+                        else
+                        {
+                            tcs.SetResult(default(T));
+                        }
+                    }
+                    catch (Exception exc) { tcs.SetException(exc); }
+                    finally { if (response != null) response.Close(); }
+                }, null);
+            }
+            catch (Exception exc) { tcs.SetException(exc); }
+            return tcs.Task;
+
+        }
+    }
+}
