d9a0e7c78ebc9d2e973a7a095cde9c4a6981bbfe
DevCardude <Cardude419@users.noreply.github.com>
2014-04-17 00:31:58 -0400

    fixed "quot;" bug
    Made it so it wouldn't return any "quot;" in the text string
    it only changes "quot;" to a " ' "

--- icndb-csharp/Joke.cs
+++ icndb-csharp/Joke.cs
@@ -5,10 +5,21 @@ namespace ChuckNorris
     [DataContract]
     public class Joke
     {
+        private string _text;
+        
         [DataMember(Name="id")]
         public string Id { get; set; }
 
         [DataMember(Name="joke")]
-        public string Text { get; set; }
+        public string Text { 
+            get 
+            {
+                return _text.Replace("quot;", "'");
+            } 
+            set
+            {
+                _text = value;
+            }
+        }
     }
 }
